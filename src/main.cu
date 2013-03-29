431215d958f2b180da6c68d76ee3599fc8cd20ad
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-29 16:59:50 +0100

    An initial build system
    Adapted from NVIDIA's template samples.

---
+++ src/main.cu
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <cuda_runtime.h>
+
+int
+main(int argc, char **argv)
+{
+    printf("Hello World...\n");
+    return 0;
+}
