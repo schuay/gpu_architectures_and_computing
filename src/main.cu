39ecb921cb75841ef7fb2e7a75c1a39f581502bd
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-31 17:06:45 +0200

    Actually perform the AND operation in parallel

--- src/main.cu
+++ src/main.cu
@@ -7,6 +7,11 @@
 
 #include "util.h"
 
+#define NBLOCKS (256)
+#define NTHREADS (256)
+
+#define NITEMS (256 * 257)
+
 #define checkCudaError(val) do { _checkCudaError((val), #val, __FILE__, __LINE__); } while (0)
 
 bool
@@ -23,7 +28,11 @@ _checkCudaError(cudaError_t result, const char *func, const char *file, int line
 
 __global__ void simpleAnd(const float *lhs, const float *rhs, float *result, int nitems)
 {
-	result[0] = 1.f;
+	const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+	for (int i = tid; i < nitems; i += blockDim.x * gridDim.x) {
+		result[i] = ((lhs[i] + rhs[i]) >= 1.f) ? 1.f : 0.f;
+	}
 }
 
 int
@@ -31,24 +40,36 @@ main(int argc, char **argv)
 {
     printf("Hello World...\n");
 
-    float *fs = random_array(42, 64);
-    float *d_fs;
-    checkCudaError(cudaMalloc((void **)&d_fs, 64 * sizeof(float)));
+    float *a = random_array(42, NITEMS);
+    float *b = random_array(43, NITEMS);
+    float *c = (float *)calloc(NITEMS, sizeof(float));
+    float *devA, *devB, *devC;
 
-    checkCudaError(cudaMemcpy(d_fs, fs, 64 * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaError(cudaMalloc((void **)&devA, NITEMS * sizeof(float)));
+    checkCudaError(cudaMalloc((void **)&devB, NITEMS * sizeof(float)));
+    checkCudaError(cudaMalloc((void **)&devC, NITEMS * sizeof(float)));
 
-    printf("fs[0]: %f\n", fs[0]);
+    checkCudaError(cudaMemcpy(devA, a, NITEMS * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaError(cudaMemcpy(devB, b, NITEMS * sizeof(float), cudaMemcpyHostToDevice));
 
     printf("Launching kernel...\n");
-    simpleAnd<<<1, 1>>>(d_fs, d_fs, d_fs, 64);
-    checkCudaError(cudaDeviceSynchronize());
+    simpleAnd<<<NBLOCKS, NTHREADS>>>(devA, devB, devC, NITEMS);
 
-    checkCudaError(cudaMemcpy(fs, d_fs, 64 * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaError(cudaMemcpy(a, devA, NITEMS * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaError(cudaMemcpy(b, devB, NITEMS * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaError(cudaMemcpy(c, devC, NITEMS * sizeof(float), cudaMemcpyDeviceToHost));
 
-    printf("fs[0]: %f\n", fs[0]);
+    for (int i = 0; i < 20; i++) {
+    	printf("%f & %f = %f\n", a[i], b[i], c[i]);
+    }
 
-    checkCudaError(cudaFree(d_fs));
-    free(fs);
+    checkCudaError(cudaFree(devA));
+    checkCudaError(cudaFree(devB));
+    checkCudaError(cudaFree(devC));
+
+    free(a);
+    free(b);
+    free(c);
 
     return 0;
 }
