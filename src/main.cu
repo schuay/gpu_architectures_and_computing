451e619a5bd75ce6177b0cfa71dffb5ebd4f2119
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-13 18:17:49 +0200

    Create the time sequence as proposed

--- src/main.cu
+++ src/main.cu
@@ -54,7 +54,7 @@ stl_not(const sigpt_t *in, sigpt_t *out, int n)
 }
 
 #define FLAG_LHS (1 << 0)
-#define FLAG_LHS (1 << 1)
+#define FLAG_RHS (1 << 1)
 #define FLAG_ISC (1 << 2)
 #define FLAG_DEL (1 << 3)
 
@@ -63,21 +63,34 @@ typedef struct {
     int flags;
 } seqpt_t;
 
-struct sigpt_less : public thrust::binary_function<sigpt_t, sigpt_t, bool>
+struct seqpt_less : public thrust::binary_function<seqpt_t, seqpt_t, bool>
 {
-    __host__ __device__ bool
-    operator()(const sigpt_t &lhs, const sigpt_t &rhs) const
+    __device__ bool
+    operator()(const seqpt_t &lhs, const seqpt_t &rhs) const
     {
         return lhs.t < rhs.t;
     }
 };
 
+__global__ void
+time_seq_with_proto_intersections(const sigpt_t *in, seqpt_t *out, int n)
+{
+    const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        float t = in[i].t;
+        out[i * 2].t = t;
+        out[i * 2 + 1].t = t;
+        out[i * 2 + 1].flags |= FLAG_ISC;
+    }
+}
+
 /**
  * sizeof(out) = 4 * max(sizeof(lhs), sizeof(rhs)).
  */
 void
-stl_and(const thrust::device_vector<sigpt_t> lhs,
-        const thrust::device_vector<sigpt_t> rhs,
+stl_and(const thrust::device_vector<sigpt_t> &lhs,
+        const thrust::device_vector<sigpt_t> &rhs,
         thrust::device_vector<sigpt_t> out)
 {
     /* Construct a sorted time sequence ts which contains all t <- lhs, all t <- rhs, and all
@@ -96,21 +109,45 @@ stl_and(const thrust::device_vector<sigpt_t> lhs,
      *   but this is also not as simple as it seems at first.
      */
 
-    const int nts = 4 * MAX(lhs.size(), rhs.size());
-    sigpt_t init = { 0.f, 0.f, 0.f };
-    thrust::device_vector<sigpt_t> ts(nts, init);
-
-    thrust::merge(lhs.begin(), lhs.end(),
-            rhs.begin(), rhs.end(),
-            ts.begin(),
-            sigpt_less());
-
-    /* The previous code demonstrates how to do a merge with thrust. 
-     * What we could now do is create two vectors of seqpt_t lhst, rhst such that
+    /* What we could now do is create two vectors of seqpt_t lhst, rhst such that
      * lhst[0] ~ lhs[0], flagged LHS; lhst[1] ~ lhs[0], flagged ISC (intersection),
      * lhst[2] ~ lhs[1], ...
-     * We then merge both of these vectors.
-     * Next, we go through and fill in ISC elements; if there's an intersection
+     */
+
+    const sigpt_t *ptr_lhs = thrust::raw_pointer_cast(lhs.data());
+
+    seqpt_t seqinit = { 0.f, FLAG_LHS };
+    thrust::device_vector<seqpt_t> lhst(lhs.size() * 2, seqinit);
+    seqpt_t *ptr_lhst = thrust::raw_pointer_cast(lhst.data());
+
+    time_seq_with_proto_intersections<<<NBLOCKS, NTHREADS>>>(ptr_lhs, ptr_lhst, lhs.size());
+
+
+    const sigpt_t *ptr_rhs = thrust::raw_pointer_cast(rhs.data());
+
+    seqinit.flags = FLAG_RHS;
+    thrust::device_vector<seqpt_t> rhst(rhs.size() * 2, seqinit);
+    seqpt_t *ptr_rhst = thrust::raw_pointer_cast(rhst.data());
+
+    time_seq_with_proto_intersections<<<NBLOCKS, NTHREADS>>>(ptr_rhs, ptr_rhst, rhs.size());
+
+    /* We then merge both of these vectors. */
+
+    const int nts = lhst.size() + rhst.size();
+    thrust::device_vector<seqpt_t> ts(nts, seqinit);
+
+    thrust::merge(lhst.begin(), lhst.end(),
+            rhst.begin(), rhst.end(),
+            ts.begin(),
+            seqpt_less());
+
+    for (thrust::device_vector<seqpt_t>::iterator iter = ts.begin();
+         iter != ts.begin() + 10; iter++) {
+    	seqpt_t s = *iter;
+    	printf("{ %f, %x }\n", s.t, s.flags);
+    }
+
+    /* Next, we go through and fill in ISC elements; if there's an intersection
      * we set the time accordingly, and if there isn't, we mark it as DEL.
      * An intersection must always be between the latest (lhs, rhs) and the next such
      * pair.
