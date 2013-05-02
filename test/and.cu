6732d524d93303b43c9f357dfae05ac013952297
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-02 17:03:01 +0200

    An initial check testcase

--- test/and.cu
+++ test/and.cu
@@ -1,13 +1,15 @@
 #include "stl.h"
 
 extern "C" {
+#include <check.h>
+
 #include "sigpt.h"
 #include "util.h"
 }
 
 #define NITEMS (42)
 
-int main(int argc, char **argv)
+START_TEST(test_sanity)
 {
     sigpt_t *a = sigpt_random(42, NITEMS);
     sigpt_t *b = sigpt_random(43, NITEMS);
@@ -22,6 +24,32 @@ int main(int argc, char **argv)
     free(a);
     free(b);
     free(c);
-    
-    return 0;
+}
+END_TEST
+
+static Suite *
+create_suite(void)
+{
+    Suite *s = suite_create(__FILE__);
+    TCase *tc_core = tcase_create("core");
+
+    tcase_add_test(tc_core, test_sanity);
+
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int
+main(int argc __attribute__ ((unused)),
+     char **argv __attribute__ ((unused)))
+{
+    int number_failed;
+    Suite *s = create_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
