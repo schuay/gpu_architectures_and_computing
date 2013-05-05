c98c49467c94c5db462b1d640f315e0408d046eb
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-05 15:56:05 +0200

    Implement sigcmp()

--- test/and.cu
+++ test/and.cu
@@ -2,6 +2,7 @@
 
 extern "C" {
 #include <check.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -12,11 +13,41 @@ extern "C" {
 
 #define NITEMS (42)
 
-int
+#define FLOAT_EQUALS(x, y) (fabs((x) - (y)) < 0.00005)
+
+static int
 sigcmp(const sigpt_t *lhs, const sigpt_t *rhs, int n)
 {
-    /* TODO: Won't work, fix me once we have decent test files. */
-    return memcmp(lhs, rhs, n);
+    int i = 0, j = 0;
+    const sigpt_t *l, *r;
+
+    /* TODO: One signal shorter than other. */
+
+    while (i < n && j < n) {
+        l = lhs + i;
+        r = rhs + j;
+
+        /* Note: dy is ignored for now because it's missing in the breach signal traces. */
+        if (!FLOAT_EQUALS(l->t, r->t) ||
+                !FLOAT_EQUALS(l->y, r->y)) {
+            fprintf(stderr, "lhs[%d]: { t: %f, y: %f, dy: %f } != "
+                            "rhs[%d]: { t: %f, y: %f, dy: %f }\n",
+                    i, l->t, l->y, l->dy,
+                    j, r->t, r->y, r->dy);
+            return -1;
+        }
+
+        do {
+            i++;
+        } while (i < n && FLOAT_EQUALS(l->y, lhs[i].y));
+
+        do {
+            j++;
+        } while (j < n && FLOAT_EQUALS(r->y, rhs[j].y));
+
+    }
+
+    return 0;
 }
 
 START_TEST(test_sanity)
