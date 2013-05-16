426819f28fb844523daed0add57d0fbad732afc3
c553db14 <c553db14@example.com>
2013-05-16 14:19:55 +0200

    Add comments for evtl

--- src/operators/evtl.cu
+++ src/operators/evtl.cu
@@ -15,6 +15,13 @@ struct sigpt_max : public thrust::binary_function<sigpt_t, sigpt_t, sigpt_t>
     }
 };
 
+/* Stores all elements zs[i] at zs_intersect[i * 2] and uses zs and ys to
+ * interpolate points at zs_intersect[i * 2 + 1] if needed.
+ * For every position in zs_intersect that contains a valid point the
+ * according position in cs is set to 1.
+ * ys: the original signal.
+ * zs: the signal after the initial scan.
+ */
 __global__ static void
 evtl_intersect(const sigpt_t *ys,
                sigpt_t *zs,
@@ -38,6 +45,7 @@ evtl_intersect(const sigpt_t *ys,
     }
 }
 
+/* Store each element zs[i] where cs[i] is 1 in zs_final[fs[i]]. */
 __global__ static void
 evtl_compact(const sigpt_t *zs,
              sigpt_t *zs_final,
@@ -67,6 +75,7 @@ stl_evtl(const thrust::device_ptr<sigpt_t> &in,
     thrust::reverse_iterator<thrust::device_ptr<sigpt_t> > rin(in + nin);
     thrust::reverse_iterator<thrust::device_ptr<sigpt_t> > rout(dout + dnout);
 
+    /* Propagate max rubustness backwards */
     thrust::inclusive_scan(rin, rin + nin, rout + nin, sigpt_max()); 
 
     const sigpt_t *ys = in.get();
@@ -76,11 +85,17 @@ stl_evtl(const thrust::device_ptr<sigpt_t> &in,
 
     sigpt_t *zs_final = dout.get();
 
+    /* Used positions in zs (out_intersect) are marked here.
+     * This amounts to all positions where there was an original value plus
+     * those where we had to interpolate a value. */
     thrust::device_vector<char> used(dnout, 0);
     char *cs = thrust::raw_pointer_cast(used.data());
     evtl_intersect<<<NBLOCKS, NTHREADS>>>(ys, zs_final, zs, cs, nin);
 
     thrust::device_vector<size_t> positions(dnout, 0);
+    /* Scan over the used positions in cs and store the result in positions
+     * For a point in zs[i], positions[i] will now contain the index at
+     * which that point should end up in the result array. */
     thrust::exclusive_scan(used.cbegin(), used.cend(), positions.begin(), 0, thrust::plus<size_t>()); 
 
     size_t *fs = thrust::raw_pointer_cast(positions.data());
