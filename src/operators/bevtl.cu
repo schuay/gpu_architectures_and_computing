4e7dfecb74747e1a4c34f53622dee133694852df
c553db14 <c553db14@example.com>
2013-06-04 17:37:11 +0200

    Some work in bevtl

--- src/operators/bevtl.cu
+++ src/operators/bevtl.cu
@@ -4,6 +4,50 @@
 
 #include "globals.h"
 
+#include <thrust/merge.h>
+#include <thrust/scan.h>
+
+typedef struct {
+    int i;
+    float t;
+    int count;
+} idx_time;
+
+__global__ static void
+to_idx_time(const sigpt_t *y,
+            const int n,
+            idx_time *out,
+            const int ofs,
+            const int count);
+
+__global__ static void
+extract_count(const idx_time *in,
+              const int n,
+              int *out,
+              int *outi);
+
+__global__ static void
+compact(const int *xs,
+        const int n,
+        const int *cs,
+        const int *fs,
+        int *out);
+
+__global__ static void
+calc_max_win_size(const int *sofs,
+                  const int *tofs,
+                  const int n,
+                  int *wsizes);
+
+struct idx_time_less : public thrust::binary_function<idx_time, idx_time, bool>
+{
+    __device__ bool
+    operator()(const idx_time &lhs, const idx_time &rhs) const
+    {
+        return lhs.t < rhs.t;
+    }
+};
+
 /**
  * Computes the running maximum of the signal y
  * with the given window size.
@@ -16,6 +60,12 @@ bevtl_rmax(const sigpt_t *y,
 
 #include <stdio.h> // TODO Remove me.
 
+static void
+time_to_idx_ofs(const thrust::device_ptr<sigpt_t> &in,
+                const int nin,
+                const float ofs,
+                thrust::device_ptr<int> *out);
+
 void
 stl_bevtl(const thrust::device_ptr<sigpt_t> &in,
           const int nin,
@@ -26,10 +76,35 @@ stl_bevtl(const thrust::device_ptr<sigpt_t> &in,
 {
     assert(s <= t);
 
-    thrust::device_ptr<float> maxs = thrust::device_malloc<float>(nin);
-    bevtl_rmax<<<NBLOCKS, NTHREADS>>>(in.get(), t - s + 1, maxs.get(), nin);
+    /* TODO: insert intersections at start and then consolidate with input. */
+
+    /* Get start and end indices of the windows. */
+    thrust::device_ptr<int> sofs = thrust::device_malloc<int>(nin);
+    time_to_idx_ofs(in, nin, s, &sofs);
+    thrust::device_ptr<int> tofs = thrust::device_malloc<int>(nin);
+    time_to_idx_ofs(in, nin, t, &tofs);
 
     thrust::host_vector<sigpt_t> h(in, in + nin);
+    sigpt_print("in", h.data(), nin);
+    thrust::host_vector<int> hs(sofs, sofs + nin);
+    thrust::host_vector<int> ht(tofs, tofs + nin);
+    for (int i = 0; i < nin; i++) {
+        printf("i: %d, t: %f, t+s: %f, i+s: %d\n", i, h[i].t, h[i].t + s, hs[i]);
+        printf("i: %d, t: %f, t+t: %f, i+t: %d\n", i, h[i].t, h[i].t + t, ht[i]);
+    }
+
+    /* Calculate the window size for each element. */
+    thrust::device_ptr<int> wsizes = thrust::device_malloc<int>(nin);
+    calc_max_win_size<<<NBLOCKS, NTHREADS>>>(sofs.get(), tofs.get(), nin, wsizes.get());
+
+    /* Calculate the position in the result array where each window will reside. */
+    thrust::exclusive_scan(wsizes, wsizes + nin, wsizes, 0, thrust::plus<int>()); 
+
+    /* TODO: calculate the evtl. */
+
+    //thrust::device_ptr<float> maxs = thrust::device_malloc<float>(nin);
+    //bevtl_rmax<<<NBLOCKS, NTHREADS>>>(in.get(), t - s + 1, maxs.get(), nin);
+
     /* TODO: Remove me.
     sigpt_print("in", h.data(), nin);
 
@@ -39,6 +114,108 @@ stl_bevtl(const thrust::device_ptr<sigpt_t> &in,
         printf("%d: %f\n", i, f);
     }
     */
+
+    thrust::device_free(sofs);
+    thrust::device_free(tofs);
+    thrust::device_free(wsizes);
+}
+
+static void
+time_to_idx_ofs(const thrust::device_ptr<sigpt_t> &in,
+                const int nin,
+                const float ofs,
+                thrust::device_ptr<int> *out)
+{
+    thrust::device_ptr<idx_time> nt = thrust::device_malloc<idx_time>(nin);
+    to_idx_time<<<NBLOCKS, NTHREADS>>>(in.get(), nin, nt.get(), 0, 1);
+
+    thrust::device_ptr<idx_time> st = thrust::device_malloc<idx_time>(nin);
+    to_idx_time<<<NBLOCKS, NTHREADS>>>(in.get(), nin, st.get(), ofs, 0);
+
+    int sntn = nin * 2;
+    thrust::device_ptr<idx_time> snt = thrust::device_malloc<idx_time>(sntn);
+    thrust::merge(nt, nt + nin, st, st + nin, snt, idx_time_less());
+
+    thrust::device_free(nt);
+    thrust::device_free(st);
+
+    thrust::device_ptr<int> c = thrust::device_malloc<int>(sntn);
+    thrust::device_ptr<int> ci = thrust::device_malloc<int>(sntn);
+
+    extract_count<<<NBLOCKS, NTHREADS>>>(snt.get(), sntn, c.get(), ci.get());
+    thrust::device_free(snt);
+
+    thrust::exclusive_scan(c, c + sntn, c, 0, thrust::plus<int>()); 
+
+    thrust::device_ptr<int> fs = thrust::device_malloc<int>(sntn);
+    thrust::exclusive_scan(ci, ci + sntn, fs, 0, thrust::plus<int>()); 
+
+    thrust::device_ptr<int> sofs = thrust::device_malloc<int>(nin);
+    compact<<<NBLOCKS, NTHREADS>>>(c.get(), sntn, ci.get(), fs.get(), sofs.get());
+
+    thrust::device_free(c);
+    thrust::device_free(ci);
+    thrust::device_free(fs);
+
+    *out = sofs;
+}
+
+__global__ static void
+calc_max_win_size(const int *sofs,
+                  const int *tofs,
+                  const int n,
+                  int *wsizes)
+{
+    const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        wsizes[i] = (tofs[i] - sofs[i]);
+    }
+}
+
+__global__ static void
+compact(const int *xs,
+        const int n,
+        const int *cs,
+        const int *fs,
+        int *out)
+{
+    const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        if (cs[i] == 1) {
+            out[fs[i]] = xs[i];
+        }
+    }
+}
+
+__global__ static void
+to_idx_time(const sigpt_t *y,
+            const int n,
+            idx_time *out,
+            const int ofs,
+            const int count)
+{
+    const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        idx_time it = { i, y[i].t + ofs, count };
+        out[i] = it;
+    }
+}
+
+__global__ static void
+extract_count(const idx_time *in,
+              const int n,
+              int *out,
+              int *outi)
+{
+    const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        out[i] = in[i].count;
+        outi[i] = ! in[i].count;
+    }
 }
 
 __global__ static void
